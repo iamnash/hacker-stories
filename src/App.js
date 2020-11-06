d5fcda4378009d1db25fba3c722d206152e2a948
iamnash <jmenashy@gmail.com>
2020-11-06 13:20:49 -0500

    complete exercise

--- src/App.js
+++ src/App.js
@@ -19,18 +19,23 @@ const list = [
   },
 ];
 
-const App = () => (
+const App = () => {
+  const handleChange = event => {
+    console.log(event.target.value);
+  };
+  
+  return (
     <div>
       <h1>My Hacker Stories</h1>
 
       <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
+      <input id="search" type="text" onChange={handleChange} />
 
       <hr />
 
       <List />     
     </div>
-  );
+  )};
 
 const List = () =>
  list.map(item => (
@@ -42,7 +47,8 @@ const List = () =>
      <span>{item.num_comments}</span>
      <span>{item.points}</span>
     </div>
-  ));
+  )
+);
  
 
 export default App;
